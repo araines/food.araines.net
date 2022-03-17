7eda55ff2debe2d04b3e5f4b033f60e0949a7a35
Andy Raines <andrew.raines@news.co.uk>
2022-03-17 20:09:05 +0000

    Add API Gateway

--- launcher.tf
+++ launcher.tf
@@ -109,6 +109,59 @@ resource "aws_iam_role_policy_attachment" "launcher_ecs" {
   policy_arn = aws_iam_policy.launcher_ecs.arn
 }
 
+resource "aws_apigatewayv2_api" "launcher" {
+  name          = "${local.site_name}-launcher"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "launcher" {
+  api_id      = aws_apigatewayv2_api.launcher.id
+  name        = "${local.site_name}-launcher"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.launcher_api_gw.arn
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "launcher" {
+  api_id             = aws_apigatewayv2_api.launcher.id
+  integration_uri    = aws_lambda_function.launcher.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "launcher" {
+  api_id    = aws_apigatewayv2_api.launcher.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.launcher.id}"
+}
+
+resource "aws_cloudwatch_log_group" "launcher_api_gw" {
+  name              = "/aws/api_gw/${aws_apigatewayv2_api.launcher.name}"
+  retention_in_days = 7
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.launcher.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.launcher.execution_arn}/*/*"
+}
 # TODO: Create an API Gateway to trigger the lambda
 # Create a Route 53 record for the API Gateway
 # Enhance Lambda to include two pieces of functionality:
